bcc4278b5c2d73bc6e0796251ef42f68b0b8633c
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-03-07 13:10:09 +0300

    Import already written library code

---
+++ src/Pipedrive.net/Models/Response/Deals/Deal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Pipedrive.CustomFields;
+using Pipedrive.Internal;
+
+namespace Pipedrive
+{
+    [JsonConverter(typeof(CustomFieldConverter))]
+    public class Deal : AbstractDeal<UserCustomField, PersonCustomField, OrganizationCustomField>, IEntityWithCustomFields
+    {
+        [JsonIgnore]
+        public IDictionary<string, ICustomField> CustomFields { get; set; }
+
+        public DealUpdate ToUpdate()
+        {
+            return new DealUpdate
+            {
+                Title = Title,
+                Value = Value,
+                Currency = Currency,
+                UserId = UserId?.Value,
+                PersonId = PersonId?.Value,
+                OrgId = OrgId?.Value,
+                StageId = StageId,
+                Status = Status,
+                Probability = Probability,
+                LostReason = LostReason,
+                VisibleTo = VisibleTo,
+                AddTime = AddTime,
+                CloseTime = CloseTime,
+                LostTime = LostTime,
+                FirstWonTime = FirstWonTime,
+                WonTime = WonTime,
+                ExpectedCloseDate = ExpectedCloseDate,
+                CustomFields = CustomFields
+            };
+        }
+    }
+}
